c868d7af99c3c477152578de49df031a00940fc6
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-27 18:16:02 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+-graphql is an alternative to rest api in a sense that graphql construct query to extract required information between client and server.
+-like restapi, graphql doesnot need to use different interfaces to retrieve different data that are requested using various request method.for example, graphql uses same interfaces with POST method for request to retrieve various required data only.
+Apollo-server<->graphql server
---
+++ index.js
@@ -0,0 +1,62 @@
+//ApolloServer class from the apollo server package is used to create server
+
+const { ApolloServer } = require('@apollo/server')
+const { startStandaloneServer } = require('@apollo/server/standalone')
+
+let persons = [
+  {
+    name: 'Arto Hellas',
+    phone: '040-123543',
+    street: 'Tapiolankatu 5 A',
+    city: 'Espoo',
+    id: '3d594650-3436-11e9-bc57-8b80ba54c431',
+  },
+  {
+    name: 'Matti Luukkainen',
+    phone: '040-432342',
+    street: 'Malminkaari 10 A',
+    city: 'Helsinki',
+    id: '3d599470-3436-11e9-bc57-8b80ba54c431',
+  },
+  {
+    name: 'Venla Ruuska',
+    street: 'Nallemäentie 22 C',
+    city: 'Helsinki',
+    id: '3d599471-3436-11e9-bc57-8b80ba54c431',
+  },
+]
+
+const typeDefs = `
+  type Person {
+    name: String!
+    phone: String
+    street: String!
+    city: String! 
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person!]!
+    findPerson(name: String!): Person
+  }
+`
+//resolvers is needed to resolve the defined schema object field path
+const resolvers = {
+  Query: {
+    personCount: () => persons.length, //personCount query resolves to function returning person length
+    allPersons: () => persons,
+    findPerson: (root, args) => persons.find((p) => p.name === args.name),
+  },
+}
+// server is created using ApolloServer which takes typedefs as string parameter and resolver is an object
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
